f82c75be7f1ef5a8223a29921a23abc1fb2beb97
Luís Falcão <lfalcao@cc.isel.pt>
2018-05-11 15:13:00 +0100

    Aula 2018-05-11 - Trab2 Join operation implementation.

--- mpdLib/src/main/java/pt/isel/mpd/v1718/li42d/stream/Queries.java
+++ mpdLib/src/main/java/pt/isel/mpd/v1718/li42d/stream/Queries.java
@@ -1,7 +1,9 @@
 package pt.isel.mpd.v1718.li42d.stream;
 
 import java.util.*;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -13,6 +15,17 @@ public class Queries {
 
     public static <T> Stream<T> distinct(Stream<T> src) {
         return StreamSupport.stream(new DistinctSpliterator(src.spliterator()), false);
+    }
+
+    public static <T, U, K, R> Stream<R> join(Stream<T> str1, Stream<U> str2, BiFunction<T, U, R> mapper, Function<T, K> tkExtractor, Function<U, K> ukExtractor) {
+        return StreamSupport.stream(new JoinSpliterator(str1.spliterator(), str2.spliterator(), mapper, tkExtractor, ukExtractor), false);
+    }
+
+    public static <T, U, K, R> Stream<R> join1(Stream<T> str1, Stream<U> str2, BiFunction<T, U, R> mapper, Function<T, K> tkExtractor, Function<U, K> ukExtractor) {
+        final Stream<Pair<T,U, K>> stream = StreamSupport.stream(new JoinInPairsSpliterator(str1.spliterator(), str2.spliterator(),  tkExtractor, ukExtractor), false)
+                .distinct();
+        return stream
+                .map(pair -> mapper.apply(pair.t, pair.u));
     }
 
     private static class CollapseSpliterator<T> extends Spliterators.AbstractSpliterator<T> {
@@ -58,6 +71,141 @@ public class Queries {
                 }
             }) && size == distinctElements.size());
             return size != distinctElements.size();
+        }
+    }
+
+
+    private static class JoinSpliterator<T, U, K, R> extends Spliterators.AbstractSpliterator<R> {
+        private final Spliterator<T> spliterator1;
+        private final Spliterator<U> spliterator2;
+        private final BiFunction<T, U, R> mapper;
+        private final Function<T, K> tkExtractor;
+        private final Function<U, K> ukExtractor;
+        private Map<K, Pair<T, U, K>> mappedValues;
+        private Spliterator<Pair<T, U, K>> mappedValuesSpliterator;
+
+
+        public JoinSpliterator(Spliterator<T> spliterator1, Spliterator<U> spliterator2, BiFunction<T, U, R> mapper, Function<T, K> tkExtractor, Function<U, K> ukExtractor) {
+            super(spliterator1.estimateSize()+spliterator2.estimateSize(), spliterator1.characteristics() & spliterator2.characteristics() & ~Spliterator.SIZED);
+
+            this.spliterator1 = spliterator1;
+            this.spliterator2 = spliterator2;
+            this.mapper = mapper;
+            this.tkExtractor = tkExtractor;
+            this.ukExtractor = ukExtractor;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super R> action) {
+            if(mappedValues == null) {
+                fillMappedValues();
+            }
+
+            if(advanceInSpliterator1(action)) {
+                return true;
+            }
+
+            return advanceInMappedValuesSpliterator(action);
+        }
+
+        private boolean advanceInMappedValuesSpliterator(Consumer<? super R> action) {
+            if(mappedValuesSpliterator == null)
+                mappedValuesSpliterator = mappedValues
+                        .values()
+                        .stream()
+                        .filter(p -> p.t == null)
+                        .spliterator();
+
+            return mappedValuesSpliterator.tryAdvance(
+                    pair -> action.accept(mapper.apply(pair.t, pair.u)));
+        }
+
+        private boolean advanceInSpliterator1(Consumer<? super R> action) {
+            final boolean[] joined = {false};
+            while (spliterator1.tryAdvance(
+                    t -> joined[0] = joinPair(t, action)) && !joined[0]);
+            return joined[0];
+        }
+
+        private boolean joinPair(T t, Consumer<? super R> action) {
+            final K key = tkExtractor.apply(t);
+            Pair<T, U, K> pair = mappedValues.get(key);
+            if(pair != null) {
+                if(pair.t == null) {
+                    pair.t = t;
+                } else {
+                    // repeated value
+                    return false;
+                }
+            } else {
+                pair = new Pair<>(t, null, key);
+                mappedValues.put(key, pair);
+            }
+            action.accept(mapper.apply(pair.t, pair.u));
+            return true;
+
+        }
+
+        private void fillMappedValues() {
+            mappedValues = new HashMap<>();
+            while (spliterator2.tryAdvance(
+                    u -> mappedValues.putIfAbsent(
+                            ukExtractor.apply(u),
+                            new Pair<>(null, u, ukExtractor.apply(u)))));
+        }
+
+    }
+
+    private static class JoinInPairsSpliterator<T, U, K> extends Spliterators.AbstractSpliterator<Pair<T,U, K>> {
+        private final Spliterator<T> spliterator1;
+        private final Spliterator<U> spliterator2;
+
+        private final Function<T, K> tkExtractor;
+        private final Function<U, K> ukExtractor;
+        private Map<K, Pair<T, U, K>> mappedValues;
+        private Spliterator<Pair<T, U, K>> mappedValuesSpliterator;
+
+
+        public JoinInPairsSpliterator(Spliterator<T> spliterator1, Spliterator<U> spliterator2, Function<T, K> tkExtractor, Function<U, K> ukExtractor) {
+            super(spliterator1.estimateSize()+spliterator2.estimateSize(), spliterator1.characteristics() & spliterator2.characteristics() & ~Spliterator.SIZED);
+
+            this.spliterator1 = spliterator1;
+            this.spliterator2 = spliterator2;
+            this.tkExtractor = tkExtractor;
+            this.ukExtractor = ukExtractor;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Pair<T,U, K>> action) {
+            return false;
+        }
+
+    }
+
+    private static class Pair<T, U, K> {
+        public T t;
+        public U u;
+        public K k;
+
+        public Pair(T t, U u, K k) {
+            this.t = t;
+            this.u = u;
+            this.k = k;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair<?, ?, ?> pair = (Pair<?, ?, ?>) o;
+
+            return k.equals(pair.k);
+        }
+
+        @Override
+        public int hashCode() {
+            return k.hashCode();
         }
     }
 }
--- mpdLib/src/test/java/pt/isel/mpd/v1718/li42d/misc/streams/StreamTest.java
+++ mpdLib/src/test/java/pt/isel/mpd/v1718/li42d/misc/streams/StreamTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import pt.isel.mpd.v1718.li42d.stream.Queries;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
@@ -89,5 +91,64 @@ public class StreamTest {
 
         assertIterableEquals(asList(1,2,null,1), res);
 
+    }
+
+    @Test
+    public void shouldJoinStreamsWithAllMergedElements() {
+
+        final List<String> strings = asList("a", "ab", "abcd", "abc");
+        final List<Integer> numbers = asList(2, 1, 3, 4);
+
+        shouldJoin(strings, numbers, asList("a(1)", "ab(2)", "abcd(4)", "abc(3)"));
+    }
+
+
+    @Test
+    public void shouldJoinStreamsWithNoneMergedElements() {
+
+        final List<String> strings = asList("a", "ab", "abcd", "abc");
+        final List<Integer> numbers = asList(5,6,7);
+
+        shouldJoin(strings, numbers, asList("a()", "ab()", "abcd()", "abc()", "#(5)", "#(6)", "#(7)"));
+    }
+
+    @Test
+    public void shouldJoinStreamsWithSomeMergedElements() {
+
+        final List<String> strings = asList("a", "ab", "abcd", "abc");
+        final List<Integer> numbers = asList(3,4,5,6,7);
+
+        shouldJoin(strings, numbers, asList("a()", "ab()", "abcd(4)", "abc(3)", "#(5)", "#(6)", "#(7)"));
+    }
+
+    @Test
+    public void shouldJoinStreamsWithSomeMergedElementsIgnoringRepeatedElements() {
+
+        final List<String> strings = asList("a", "ab", "a", "abc", "ab");
+        final List<Integer> numbers = asList(3,4,5,6,4,3);
+
+        shouldJoin(strings, numbers, asList("a()", "ab()", "abc(3)", "#(4)", "#(5)", "#(6)"));
+    }
+
+
+    private void shouldJoin(List<String> strings, List<Integer> numbers, List<String> expected) {
+
+
+        final Stream<String> res = Queries.join(
+                strings.stream(),
+                numbers.stream(),
+                this::mapStringWithInteger,
+                String::length,
+                Function.identity()
+        );
+
+        assertIterableEquals(expected, res.collect(toList()));
+    }
+
+    private String mapStringWithInteger(String s, Integer i) {
+        String str1 = s == null ? "#" : s;
+        String str2 = i == null ? "" : i.toString();
+
+        return str1 + "(" + str2 + ")";
     }
 }
